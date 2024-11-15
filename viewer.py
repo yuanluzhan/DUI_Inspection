e563ca5d013b042d255c4f7743354f49aed14ec6
yuanluzhan <yuanluzhan@gmail.com>
2024-11-15 13:05:17 +0800

    上线app

--- viewer.py
+++ viewer.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from dui_scenario import scenario
 import matplotlib.pyplot as plt
 import networkx as nx
